13d616fc177b2ca173912ec4ec19455a7a265a99
OH-JOO-YEONG <brb1111@naver.com>
2019-09-18 22:59:31 +0900

    Safety website done

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,20 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {
         'post_list': posts
     })
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
@@ -17,6 +24,7 @@ def post_detail(request, pk):
         {'post': post}
     )
 
+@login_required # 로그인이 됬을 때만 그 페이지를 쓸 수 있다. #장식자 문법이라고 함
 def post_new(request):
     form = PostForm()
 
@@ -25,16 +33,17 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now() 미리 보기로 글을 저장하기 위한 장치.
             post.save()
             return redirect('post_detail', post.pk)
     else:
         form = PostForm()
 
     return render(request, 'blog/post_edit.html', {
-        'form':form,
+        'form': form,
     })
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
@@ -43,7 +52,7 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', post.pk)
     else:
@@ -54,3 +63,15 @@ def post_edit(request, pk):
         'blog/post_edit.html',
         {'form': form}
     )
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+@login_required
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
