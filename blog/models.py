00380e70e13a313a996d53904f95185634fe1706
OH-JOO-YEONG <brb1111@naver.com>
2019-09-18 23:46:00 +0900

    comment function done

--- blog/models.py
+++ blog/models.py
@@ -17,3 +17,23 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    approved_comment = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-id'] #새글이 위로 오게하는 것
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def __str__(self):
+        return self.text
